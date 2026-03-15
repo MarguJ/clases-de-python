93fba9b6f7da9f32cb4937ca8abb25392ccee48e
MarguJ <margulesjoa@gmail.com>
2026-03-15 18:46:46 -0300

    Ejercicios 1-6 hechos

---
+++ main.py
@@ -0,0 +1,70 @@
+'''
+EJERCICIO 1
+nombre = str(input("Ingrese su nombre: "))
+print("¡Hola " + nombre + "!")
+'''
+
+'''
+EJERCICIO 2
+nombre = str(input("Ingrese su nombre: "))
+apellido = str(input("Ingrese su apellido: "))
+edad = int(input("Ingrese su edad: "))
+altura = float(input("Ingrese su altura en metros: "))
+print("¡Hola, " + nombre + " " + apellido + "! Tu edad es " + str(edad) + " y tu altura es " + str(altura) + " metros.")
+'''
+
+'''
+EJERCICIO 3
+añoA = 2026
+añoDN = int(input("Ingrese su año de nacimiento: "))
+edad = añoA - añoDN
+print("Tu edad es " + str(edad) + ".")
+'''
+
+'''
+EJERCICIO 4
+numero1 = int(input("Ingrese numero 1: "))
+numero2 = int(input("Ingrese numero 2: "))
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print("La suma de esos dos numeros es = " + str(suma) + ". La resta es = " + str(resta) + ". El producto es = " + str(multiplicacion) + ". El cociente es = " + str(division) + ".")
+'''
+
+'''
+EJERCICIO 5
+frase1 = str(input("Ingrese frase 1: "))
+frase2 = str(input("Ingrese frase 2: "))
+frase3 = str(input("Ingrese frase 3: "))
+print(frase2 + ", " + frase3 + ", " + frase1 + ".")
+'''
+
+'''
+EJERCICIO 6
+base = float(input("Ingrese la base de su triángulo (en cm): "))
+altura = float(input("Ingrese la altura de su triángulo (en cm): "))
+areaDTriangulo = (base * altura)/2
+print("El area de su triangulo es = " + str(areaDTriangulo) + " centimetros.")
+'''
+
+'''
+EJERCICIO 7
+'''
+precioBruto = float(input("Ingrese precio exacto de la heladera: "))
+IVA = float(0.21)
+precioTotal = precioBruto * IVA
+print("El precio total es " + str(precioTotal))
+
+
+'''
+EJERCICIO 8
+'''
+
+'''
+EJERCICIO 9
+'''
+
+'''
+EJERCICIO 10
+'''
